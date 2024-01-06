814373c0b1b3fec5a470c32565a4e1faa33f5171
alexeicaps <caps.alex@mail.ru>
2024-01-06 13:58:22 +0200

    lesson4___Update

--- src/start/lesson2/Variables.java
+++ src/start/lesson2/Variables.java
@@ -4,10 +4,16 @@ public class Variables {
     public static void main(String[] args) {
         int countApples;
         countApples = 3;
-        System.out.print(countApples);
+        System.out.println(countApples);
 
         //Ключевое слово var
+//        var countPeople = 15;
+//        var letter = 'Б';
+
         var countPeople = 15;
-        var letter = 'Б';
+        var letter = 'А';
+        var z = countApples + letter;
+        System.out.println(z);
+
     }
 }
---
+++ src/start/lesson4___/Conditions.java
@@ -0,0 +1,18 @@
+package start.lesson4___;
+
+public class Conditions {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 5;
+
+        if (x > y) {
+            System.out.println("X bigger than Y");
+        } else if (x == y) {
+            System.out.println("X equal Y");
+        }
+        else {
+            System.out.println("Y bigger than X");
+        }
+
+    }
+}
