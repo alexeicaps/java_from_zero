cc6ab51f6f4179168a67214344d900193bfa6eb7
Alexei Capsamun <caps.alex@mail.ru>
2024-01-12 16:55:48 +0200

    add lesson6___.StringMethod update

--- src/start/lesson6___/StringMethod.java
+++ src/start/lesson6___/StringMethod.java
@@ -1,8 +1,10 @@
 package start.lesson6___;
 
+import java.util.Arrays;
+
 public class StringMethod {
     public static void main(String[] args) {
-        String text = "Hello world";
+        String text = "Hello world!";
         //Перевод в верхний регистр
         String textUpper = text.toUpperCase();
         System.out.println(textUpper);
@@ -10,5 +12,26 @@ public class StringMethod {
         //Перевод в нижний регистр
         String textLower = text.toLowerCase();
         System.out.println(textLower);
+
+        //Длина строки
+        int length = text.length();
+        System.out.println(length);
+
+        //Есть ли в тексте другой текст?
+        boolean containsHello = text.contains("Hello");
+        System.out.println(containsHello);
+
+        //Замена всех кусков текста(подстроки) на другой текст
+        String replaceText = text.replaceAll("l", "F");
+        System.out.println(replaceText);
+
+        //Повторить текст n раз
+        String repeatedText = text.repeat(5);
+        System.out.println(repeatedText);
+
+        //Разбить текст на массив String
+        String names = "Yuri;Olga;Petr;Vasya";
+        String[] namesArray = names.split(";", 3);
+        System.out.println(Arrays.toString(namesArray));
     }
 }
